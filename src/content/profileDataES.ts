138cd5dc31ffaccf3936984eba51061590cb1952
BeautifulPhoenix <oriolrafols98@gmail.com>
2025-02-20 19:42:25 +0100

    Language upgrade

---
+++ src/content/profileDataES.ts
@@ -0,0 +1,99 @@
+export const PROFILE = {
+  site: {
+    SEO: {
+      //     more complicated description for SEO
+      title: "Oriol Rafols - Colabora, crea y transforma el mundo con código abierto ❤️",
+      description:
+        "Colabora, crea y transforma el mundo con código abierto ❤️ | 💻 Administrador de Sistemas / Desarrollador Web | Portfolio",
+    },
+  },
+  timezone: "Europe/Madrid",
+  language: "Es-Es",
+  firstName: "Oriol",
+  name: "Oriol Rafols",
+  headLine: "Colabora, crea y transforma el mundo con código abierto ❤️",
+  headLine2:
+    "Administrador de Sistemas, apasionado por aprender y mejorar habilidades en ciberseguridad y programación.",
+  website: "https://oriolrafols.com",
+  repo: "https://github.com/BeautifulPhoenix",
+  avatarUrl: "/techtim42.webp",
+  shortIntros: [
+    "💻 Administrador de Sistemas / Desarrollador Web | Defensor del Código Abierto | Autodidacta | Entusiasta de la Ciberseguridad",
+    " ",
+    "🚀 Impulsado por mejorar la experiencia VR (VRX) y hacer realidad ideas inmersivas",
+    "📈 Entusiasta de Crypto y Blockchain",
+    "☁️ Apasionado del Self-Hosting | Creando Soluciones Independientes y Eficientes",
+    "🎶 Amante de la Música | 👾 Gamer | 🐉 Jugador de D&D",
+  ],
+  words: ["Cada línea de código que escribes es un paso más hacia la mejor versión de ti mismo"],
+  skills: [
+    "Python",
+    "JavaScript",
+    "HTML",
+    "C/C++",
+    "Wordpress",
+    "O365",
+    "Cloud Computing",
+    "VMware",
+    "ESXi",
+    "Proxmox",
+    "PostgresSQL",
+    "SQL",
+    "Astro",
+    "PHP",
+    "MySQL",
+    "CSS",
+    "Docker",
+    "Windows",
+    "Linux"
+  ],
+  links: {
+    github: "https://github.com/BeautifulPhoenix",
+    twitter: "https://x.com/Blue_Phoenix43",
+    linkedin: "https://www.linkedin.com/in/oriol-rafols-5bb760143/",
+    medium: "",
+    bluesky: "",
+    thread: "",
+    mastodon: "",
+    youtube: "",
+    stackoverflow: "https://stackoverflow.com/users/18226029/bluephoenix",
+    devto: "",
+    goodreads: ""
+  },
+  studies: [
+    // estudios, cualificaciones y certificaciones
+    {
+      title: "ADMINISTRADOR DE SISTEMAS Y REDES",
+      level: "CFGM",
+    },
+    {
+      title: "Sistemas Microinformáticos y Redes",
+      institution: "CFGM",
+    },
+    {
+      title: "Oracle Database Design and Programming with SQL",
+      institution: "Oracle",
+    },
+    {
+      title: "Oracle Database Programming with PL/SQL",
+      institution: "Oracle",
+    },
+    {
+      title: "CCNA R&S: Routing and Switching Essentials",
+      institution: "Cisco",
+    },
+    {
+      title: "Microsoft Windows Server 2016",
+      institution: "Microsoft",
+    },
+    {
+      title: "Cisco Networking Academy",
+      institution: "iT Essentials",
+      url: "https://cs.lpi.org/caf/Xamman/certification/verify/LPI000402228/3jhzkjchku",
+    },
+    {
+      title: "Linux Essentials",
+      institution: "lpi",
+    },
+  ],
+};
